6d9b0d385fadc12be92c600aa870a6c0aa409a2a
Abn_Only_me <126083308+ratmol@users.noreply.github.com>
2025-04-04 05:02:38 -0400

    Add files via upload

---
+++ postgresModel.js
@@ -0,0 +1,9 @@
+// Initalize 
+const Sequelize = require('sequelize');
+
+// Your Code here. Connection and Model goes here.
+
+// Export the module - "PostgresModel" is whatever you called your model. 
+// It must be named "PostgresModel" in order for it to work inside of mongoRoutes.js
+// Uncomment this to export your model to your routes.
+//module.exports = PostgresModel;
---
+++ postgresRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+// import your model
+const PostgresModel = require('../models/postgresModel');
+
+// Create a new item
+
+// Other CRUD operations (read, update, delete) ...
+
+module.exports = router;
